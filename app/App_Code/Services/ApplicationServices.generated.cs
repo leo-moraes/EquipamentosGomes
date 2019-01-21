627eb88ed5fc7dead84cef5a7fb11e29e2497865
Léo Oliveira <leoli@Notebook-MKT.gomes-castilho.com.br>
2019-01-21 12:26:24 -0200

    Adicionar arquivos de projeto.

---
+++ app/App_Code/Services/ApplicationServices.generated.cs
@@ -0,0 +1,28 @@
+﻿using FreeTrial.Handlers;
+using FreeTrial.Data;
+using FreeTrial.Web;
+using System.Web.Configuration;
+
+namespace FreeTrial.Services
+{
+	public class AppFrameworkConfig
+    {
+        
+        public virtual void Initialize()
+        {
+            ApplicationServices.FrameworkAppName = "Equipamentos Gomes Castilho";
+            ApplicationServices.Version = "8.7.8.0";
+            ApplicationServices.JqmVersion = "1.4.6";
+            ApplicationServices.HostVersion = "1.2.5.0";
+            var releaseMode = true;
+            AquariumExtenderBase.EnableMinifiedScript = releaseMode;
+            AquariumExtenderBase.EnableCombinedScript = releaseMode;
+            ApplicationServices.EnableMinifiedCss = releaseMode;
+            ApplicationServices.EnableCombinedCss = releaseMode;
+            CultureManager.SupportedCultures = new string[] {
+                    "pt-BR,pt-BR"};
+            ApplicationServicesBase.AuthorizationIsSupported = false;
+            BlobFactoryConfig.Initialize();
+        }
+    }
+}
